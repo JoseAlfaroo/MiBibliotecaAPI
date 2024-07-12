ae843de0fef3d558629648f525b1d80d791803be
Josè Alfaro <already.5.35@gmail.com>
2024-07-12 13:23:30 -0500

    CRUD Autor
    - Implementa métodos de creación, lectura, actualización y eliminación
    - Excluye objeto 'Pais' del cuerpo de petición JSON en clase Autor

--- LibraryWebAPI/Controllers/AutorController.cs
+++ LibraryWebAPI/Controllers/AutorController.cs
@@ -1,6 +1,8 @@
 ﻿using LibraryWebAPI.Context;
+using LibraryWebAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibraryWebAPI.Controllers
 {
@@ -15,5 +17,117 @@ namespace LibraryWebAPI.Controllers
             _context = context;
         }
 
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var autores = await _context.Autores.ToListAsync();
+                return Ok(autores);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.ToString() });
+            }
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var autor = await _context.Autores.FindAsync(id);
+
+                if (autor == null)
+                {
+                    return NotFound(new { message = "Autor no encontrado" });
+                }
+
+                return Ok(autor);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.ToString() });
+            }
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Autor model)
+        {
+            var autor = new Autor
+            {
+                NombresAutor = model.NombresAutor,
+                ApellidosAutor = model.ApellidosAutor,
+                PaisID = model.PaisID
+            };
+
+            try
+            {
+                _context.Autores.Add(autor);
+                await _context.SaveChangesAsync();
+
+                return Ok(autor);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.ToString() });
+            }
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] Autor model)
+        {
+            try
+            {
+                var autor = await _context.Autores.FindAsync(id);
+
+                if (autor == null)
+                {
+                    return NotFound(new { message = "Autor no encontrado" });
+                }
+
+                autor.NombresAutor = model.NombresAutor;
+                autor.ApellidosAutor = model.ApellidosAutor;
+                autor.PaisID = model.PaisID;
+
+                _context.Autores.Update(autor);
+                await _context.SaveChangesAsync();
+
+                return Ok(autor);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.ToString() });
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var autor = await _context.Autores.FindAsync(id);
+
+                if (autor == null)
+                {
+                    return NotFound(new { message = "Autor no encontrado" });
+                }
+
+                _context.Autores.Remove(autor);
+                await _context.SaveChangesAsync();
+
+                return Ok(new { message = "Autor eliminado correctamente" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.ToString() });
+            }
+        }
     }
 }
--- LibraryWebAPI/Models/Autor.cs
+++ LibraryWebAPI/Models/Autor.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace LibraryWebAPI.Models
 {
@@ -12,9 +13,12 @@ namespace LibraryWebAPI.Models
         public string? NombresAutor { get; set; }
         [Required]
         public string? ApellidosAutor { get; set; }
+        
+        // Puede ser NULL para permitir autores sin pais, tanto para listado como para registro y actualización
+        public int? PaisID { get; set; }
 
         //Relacion con pais
-        public int? PaisID { get; set; }
+        [JsonIgnore]
         public Pais? Pais { get; set; }
     }
 }
