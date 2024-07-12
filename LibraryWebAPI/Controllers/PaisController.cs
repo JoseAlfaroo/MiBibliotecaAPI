58746597669721c79fa8125d6d609d165f9b84b9
Josè Alfaro <already.5.35@gmail.com>
2024-07-12 00:24:10 -0500

    CRUD Pais
    Se agregan metodos de lectura, modificación y eliminación

--- LibraryWebAPI/Controllers/PaisController.cs
+++ LibraryWebAPI/Controllers/PaisController.cs
@@ -2,6 +2,7 @@
 using LibraryWebAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibraryWebAPI.Controllers
 {
@@ -15,6 +16,43 @@ namespace LibraryWebAPI.Controllers
         {
             _context = context;
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var paises = await _context.Paises.ToListAsync();
+                return Ok(paises);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.ToString() });
+            }
+        }
+            
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var pais = await _context.Paises.FindAsync(id);
+
+                if(pais == null)
+                {
+                    return NotFound(new { message = "Pais no encontrado" });
+                }
+
+                return Ok(pais);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.ToString() });
+            }
+        }
+
 
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] Pais model)
@@ -30,6 +68,56 @@ namespace LibraryWebAPI.Controllers
                 await _context.SaveChangesAsync();
 
                 return Ok(pais);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.ToString() });
+            }
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] Pais model)
+        {
+            try
+            {
+                var pais = await _context.Paises.FindAsync(id);
+
+                if (pais == null)
+                {
+                    return NotFound(new { message = "Pais no encontrado" });
+                }
+
+                pais.NombrePais = model.NombrePais;
+
+                _context.Paises.Update(pais);
+                await _context.SaveChangesAsync();
+
+                return Ok(pais);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.ToString() });
+            }
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var pais = await _context.Paises.FindAsync(id);
+
+                if(pais == null)
+                {
+                    return NotFound(new { mensaje = "Pais no encontrado" });
+                }
+
+                _context.Paises.Remove(pais);
+                await _context.SaveChangesAsync();
+
+                return Ok(new { message = "Pais eliminado correctamente" });
             }
             catch (Exception ex)
             {
