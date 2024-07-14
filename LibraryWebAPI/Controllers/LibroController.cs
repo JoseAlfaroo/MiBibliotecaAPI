29888edf2d24aff8935a86a0854a620652526710
Josè Alfaro <already.5.35@gmail.com>
2024-07-14 01:08:57 -0500

    Agrega métodos Genero/Autor a 'LibroController'
    - Implementa métodos para añadir Géneros y/o Autores al Libro
    - Implementa métodos para quitar Géneros y/o Autores del Libro

--- LibraryWebAPI/Controllers/LibroController.cs
+++ LibraryWebAPI/Controllers/LibroController.cs
@@ -140,6 +140,143 @@ namespace LibraryWebAPI.Controllers
         }
 
 
-        
+        // Metodo para agregar generos del libro
+        [HttpPost("{libroID}/generos/{generoID}")]
+        public async Task<IActionResult> AddGenero(int libroID, int generoID)
+        {
+            try
+            {
+                var libro = await _context.Libros
+                    .Include(l => l.GenerosLibro)
+                    .FirstOrDefaultAsync(l => l.LibroID == libroID);
+
+                if(libro == null)
+                {
+                    return NotFound(new { message = "Libro no encontrado" });
+                }
+
+                var genero = await _context.Generos.FindAsync(generoID);
+                
+                if (genero == null)
+                {
+                    return NotFound(new { message = "Genero no encontrado" });
+                }
+
+                libro.GenerosLibro?.Add(new GeneroLibro { Genero = genero });
+
+                await _context.SaveChangesAsync();
+
+                return Ok(libro);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.ToString() });
+            }
+        }
+
+
+        // Metodo para eliminar generos del libro
+        [HttpDelete("{libroID}/generos/{generoID}")]
+        public async Task<IActionResult> RemoveGenero(int libroID, int generoID)
+        {
+            try
+            {
+                var libro = await _context.Libros
+                    .Include(l => l.GenerosLibro)
+                    .FirstOrDefaultAsync(l => l.LibroID == libroID);
+
+                if (libro == null)
+                {
+                    return NotFound(new { message = "Libro no encontrado" });
+                }
+
+                var generoLibro = libro.GenerosLibro?.FirstOrDefault(gl => gl.GeneroID == generoID);
+
+                if (generoLibro == null)
+                {
+                    return NotFound(new { message = "El libro no tiene este género" });
+                }
+
+                libro.GenerosLibro?.Remove(generoLibro);
+
+                await _context.SaveChangesAsync();
+
+                return Ok(libro);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.ToString() });
+            }
+        }
+
+
+        // Metodo para agregar autores del libro
+        [HttpPost("{libroID}/autores/{autorID}")]
+        public async Task<IActionResult> AddAutor(int libroID, int autorID)
+        {
+            try
+            {
+                var libro = await _context.Libros
+                    .Include(l => l.AutoresLibro)
+                    .FirstOrDefaultAsync(l => l.LibroID == libroID);
+
+                if (libro == null)
+                {
+                    return NotFound(new { message = "Libro no encontrado" });
+                }
+
+                var autor = await _context.Autores.FindAsync(autorID);
+
+                if (autor == null)
+                {
+                    return NotFound(new { message = "Autor no encontrado" });
+                }
+
+                libro.AutoresLibro?.Add(new AutorLibro { Autor = autor });
+
+                await _context.SaveChangesAsync();
+
+                return Ok(libro);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.ToString() });
+            }
+        }
+
+
+        // Metodo para eliminar autores del libro
+        [HttpDelete("{libroID}/autores/{autorID}")]
+        public async Task<IActionResult> RemoveAutor(int libroID, int autorID)
+        {
+            try
+            {
+                var libro = await _context.Libros
+                    .Include(l => l.AutoresLibro)
+                    .FirstOrDefaultAsync(l => l.LibroID == libroID);
+
+                if (libro == null)
+                {
+                    return NotFound(new { message = "Libro no encontrado" });
+                }
+
+                var autorLibro = libro.AutoresLibro?.FirstOrDefault(gl => gl.AutorID == autorID);
+
+                if (autorLibro == null)
+                {
+                    return NotFound(new { message = "El libro no tiene este autor" });
+                }
+
+                libro.AutoresLibro?.Remove(autorLibro);
+
+                await _context.SaveChangesAsync();
+
+                return Ok(libro);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = ex.ToString() });
+            }
+        }
     }
 }
